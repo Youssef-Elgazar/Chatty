3533b67d1376d6d9e26cb62efb7d97cdf11bb48d
MuhammadSoliman14 <musoliman14@gmail.com>
2022-05-27 17:44:58 +0200

    as

---
+++ src/main/dbconnect.java
@@ -0,0 +1,36 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class dbconnect {
+
+
+
+
+        private static String url = "jdbc:mysql://127.0.0.1:3306";
+        private static String drivername = "com.mysql.jdbc.Driver";
+        private static String username = "root";
+        private static String password = "";
+        private static java.sql.Connection con;
+        private static String urlstring;
+
+        public java.sql.Connection getconnection() throws ClassNotFoundException, SQLException {
+
+            Class.forName(drivername);
+            try {
+                con = (java.sql.Connection) DriverManager.getConnection(url, username, password);
+                System.out.println("connected..");
+            } catch (Exception e) {
+                System.out.println("something went wrong..." + e);
+            }
+
+            return con;
+        }
+
+        public static void cloaseconnection() throws SQLException {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("something wrong" + e);
+            }
+        }
+
+}
---
+++ src/main/signupController.java
@@ -0,0 +1,21 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class signupController {
+    @FXML
+    private Button Signup;
+    public void signupOnAction(ActionEvent event) throws IOException {
+        Parent view3 = FXMLLoader.load(getClass().getResource("signup.fxml"));
+        Scene scene3 = new Scene(view3);
+        Stage window = new Stage();
+        window.setScene(scene3);
+        window.show();
+    }
+}
